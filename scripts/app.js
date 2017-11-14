5f8b5d34f5d316fd3f3d941391cb854bb8b2cae1
Bacastow <RMB1@BLAIRNET.NET>
2017-11-14 11:34:59 -0600

    Added fiber

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ app.controller('myController', function ($scope, $http) {
             "appId": "f4ba6075",
             "appKey": "82ea3540c0cd81efe84c090999363701",
             "query": ($scope.userInput),
-            "fields": ["item_name", "brand_name", "nf_serving_size_qty", "nf_serving_size_unit", "nf_total_carbohydrate", "nf_sugars", "nf_calories"],
+            "fields": ["item_name", "brand_name", "nf_serving_size_qty", "nf_serving_size_unit", "nf_total_carbohydrate", "nf_sugars", "nf_calories","nf_dietary_fiber"],
             "filters": {
                 "item_type": 2
             }
