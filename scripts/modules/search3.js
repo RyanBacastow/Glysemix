26b3829bfe51bd1f0e82e5600853fa6e1702c82a
RyanBacastow <washingtonirving2017@gmail.com>
2017-11-15 20:51:35 -0600

    fixed search js

--- scripts/modules/search3.js
+++ scripts/modules/search3.js
@@ -8,22 +8,31 @@ ingredientSearch.controller('search3Ctrl', function ($scope, $http) {
         };
     };
 
-    $scope.searchFunction = function(){
-    $http.post("https://api.nutritionix.com/v1_1/search", {
-        "appId": "f4ba6075",
-        "appKey": "82ea3540c0cd81efe84c090999363701",
-        "query": ($scope.userInput),
-        "fields": ["item_name", "brand_name", "nf_serving_size_qty", "nf_serving_size_unit", "nf_total_carbohydrate", "nf_sugars", "nf_calories"],
-//        "sort": {
-//            "field": "nf_sugars",
-//            "order": "desc"
-//        },
-        "filters": {
-            "item_type": 3
-        }
-    }).then(function (response) {
-        $scope.nutrition = response.data.hits;
-        //        function(err){alert("Please refresh")};
-    });
+     $scope.searchFunction = function () {
+        $http.post("https://api.nutritionix.com/v1_1/search", {
+            "appId": "f4ba6075",
+            "appKey": "82ea3540c0cd81efe84c090999363701",
+            "query": ($scope.userInput),
+            "fields": ["item_name",
+					   "brand_name",
+					   "nf_serving_size_qty",
+					   "nf_dietary_fiber",
+					   "nf_serving_size_unit",
+					   "nf_total_carbohydrate",
+					   "nf_sugars",
+					   "nf_calories"],
+			"limit": 100,
+            "filters": { 
+                "item_type": 3
+            },
+        }).then(function (response) {
+			if (response.data.hits<1){
+				alert("Sorry, I couldn't find results for this item. Maybe try switching the search to restaurants or ingredients.")}
+			else{
+				$scope.nutrition = response.data.hits};
+        });
     };
 });
+
+
+
